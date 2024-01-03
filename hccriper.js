1f3feef848dcd96ac25d759b08b3e014e29ef64a
vandupio2022 <115166676+vandupio2022@users.noreply.github.com>
2024-01-03 20:09:06 +0800

    Create hccriper.js

---
+++ hccriper.js
@@ -0,0 +1,58 @@
+function canopen(){
+	var values = ["60000","50000","55000","20000"];
+	var click = document.querySelectorAll(".py-3.cursor-pointer.flex.items-center.justify-center");
+	var delay = values[Math.floor(Math.random() * values.length)];
+	
+
+	for (var i=0; i<click.length;i++)
+		{
+			click[i].click();
+			setTimeout(function(){},delay);
+		}
+	i=undefined;
+}
+
+function minner(){
+var tablex = document.querySelectorAll(".min-w-full.divide-y.divide-gray-200 tbody tr");
+var result=[];
+for (var i=0; i <tablex.length;i++)
+{
+	var devicex;
+	var targets="";
+	lang = tablex[i].querySelectorAll("img");
+	var dcount = tablex[i].querySelectorAll("td")[5].querySelectorAll("svg").length;
+	if(dcount == 1)
+		{
+			devicex = "Mobile";
+		}else
+		{
+			devicex = "Desktop & Tab";
+		}
+	var sLang=[]; 
+		for (var la=0; la <lang.length; la++)
+			{
+				sLang.push(lang[la].getAttribute("alt"));
+			}
+
+	//targets
+	var target = tablex[i].querySelectorAll("td div.translate-y ul")[0].querySelectorAll("p");
+	for(var tr=0; tr<target.length;tr++)
+	{
+		targets = target[tr].innerText+"\n"+targets;
+	}
+
+	var name = tablex[i].querySelectorAll("td")[1].innerText;
+	var property= tablex[i].querySelectorAll("td")[2].innerText;
+	var state = tablex[i].querySelectorAll("td")[3].innerText;
+	var IId=tablex[i+1].querySelector(".text-xs.text-gray-400").innerText.split(": ")[1];
+	result.push(property+"\t"+name+"\t"+state+"\t"+sLang+"\t"+devicex+"\t"+IId+"\t"+targets)
+	i++;
+}
+var writedata = result.join('\n');
+console.log(writedata);
+}
+function action()
+{
+  minner();
+  canopen();
+}
